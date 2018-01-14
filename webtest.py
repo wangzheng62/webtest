111f69bdee9e6cb809081aa7efb0cec4ca2b0774
wangzheng62 <363579018@qq.com>
2018-01-14 16:53:32 +0800

    11

--- webtest.py
+++ webtest.py
@@ -1,9 +1,9 @@
-from flask import Flask,redirect,url_for,render_template
+from flask import Flask,redirect,url_for,render_template,request,flash
 from alarmclock import alarmclock
 import time
 
 app = Flask(__name__)
-
+app.secret_key = 'some_secret'
 
 @app.route('/')
 def index():
@@ -13,8 +13,15 @@ def form01():
     return render_template('form01.html')
 @app.route('/echarts01')
 def echarts01():
-    return render_template('echarts01.html')
-
+    return render_template('echart01.html')
+@app.route('/list01')
+def list01():
+    return render_template('list01.html')
+@app.route('/getform',methods=["get","post"])
+def getform():
+    a=request.form
+    flash("提交成功")
+    return redirect(url_for("form01"))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
