8c7fe1d45372b5e4beaa04822a7f46011dbfa861
wangzheng62 <363579018@qq.com>
2018-01-19 16:50:39 +0800

    222

--- webtest.py
+++ webtest.py
@@ -1,6 +1,5 @@
 from flask import Flask,redirect,url_for,render_template,request,flash
-from alarmclock import alarmclock
-import time
+from func import DBserver,Groupdata1,Group10
 
 app = Flask(__name__)
 app.secret_key = 'some_secret'
@@ -10,21 +9,40 @@ def index():
     return render_template('index.html')
 @app.route('/form01')
 def form01():
-    return render_template('form01.html')
+    res=Group10.colnames()
+    return render_template('form01.html',data=res)
+@app.route('/search01')
+def search01():
+    names=Group10.colnames()
+    return render_template('search01.html',formdata=names)
 @app.route('/echarts01')
 def echarts01():
     return render_template('echart01.html')
 @app.route('/list01')
 def list01():
-    return render_template('list01.html')
+    res=Group10.fetchall(10)
+    res.insert(0,Group10.colnames())
+    return render_template('list01.html',data=res)
 @app.route('/getform',methods=["get","post"])
 def getform():
     a=request.form
-    if save(a):
+    l=Group10(**a)
+    print(l.info)
+    if l.save():
         flash("提交成功")
     else:
         flash("提交失败")
     return redirect(url_for("form01"))
+@app.route('/search',methods=["get","post"])
+def search():
+    a=request.form
+    l=Group10(**a)
+    res=l.search()
+    names=l.colnames()
+    if res==[]:
+        flash('没有查到结果')
+    res.insert(0,names)
+    return render_template('search01.html',formdata=names, listdata=res)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
